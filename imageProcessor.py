08f96b6c625f078558ef5eab09d08c8f255e1277
iamzulkarnine <iamzulkarnine@gmail.com>
2021-04-24 05:44:28 +0600

    Accepting json image now

--- imageProcessor.py
+++ imageProcessor.py
@@ -39,3 +39,18 @@ class ImageProcessor:
         # Convert image into numpy format
         numpy_image = np.array(pil_image).reshape((SIZE, SIZE, 3))
         return numpy_image
+
+    def process_imageform(self,image,SIZE):
+        pil_image = Image.open(io.BytesIO(image))
+        print(pil_image)
+        if pil_image.mode == 'RGBA':
+            pil_image = pil_image.convert('RGB')
+        elif pil_image.mode == 'P':
+            pil_image = pil_image.convert('RGB')
+
+        # Resize image to expected input shape
+        pil_image = pil_image.resize((SIZE, SIZE))
+
+        # Convert image into numpy format
+        numpy_image = np.array(pil_image).reshape((SIZE, SIZE, 3))
+        return numpy_image
